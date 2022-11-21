da937e299d099e0e647b8290123a762b272a8cdd
ayau-akk <118360890+ayau-akk@users.noreply.github.com>
2022-11-21 14:52:46 +0600

    Add files via upload

---
+++ hw1.1.php
@@ -0,0 +1,13 @@
+<?php
+
+$x = 3.65;
+$y = 6.65;
+$z = -11.54;
+
+echo round($x,1);  
+echo '<br><br>';
+echo ceil($y * 100) / 100; 
+echo '<br><br>';
+echo floor($z * 100) / 100;  
+
+?>
---
+++ hw1.2.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = 4;
+$y = 5; 
+
+
+
+if(rand(1,4) == 1){
+	echo($x + $y);
+    
+} else if(rand(1,4) == 2){
+	echo($x - $y); 
+} else if(rand(1,4) == 3){
+	echo($x * $y); 
+}
+	echo($x / $y); 
+
+
+
+?>
