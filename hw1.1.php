ec843fc1b625ec23d7256aaa8983943a603e7673
ayau-akk <118360890+ayau-akk@users.noreply.github.com>
2022-11-30 20:07:28 +0600

    Update hw1.1.php
    исправленная версия с вашими коментариями)

--- hw1.1.php
+++ hw1.1.php
@@ -1,5 +1,4 @@
 <?php
-
 $x = 3.65;
 $y = 6.65;
 $z = -11.54;
@@ -8,6 +7,5 @@ echo round($x,1);
 echo '<br><br>';
 echo ceil($y * 100) / 100; 
 echo '<br><br>';
-echo floor($z * 100) / 100;  
-
-?>
+echo round($z,1);  
+?> 
